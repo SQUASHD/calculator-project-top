685452c23aeb4cecf18ddd929365752b2b7819b6
Daniel Hjartland <squashical@gmail.com>
2022-10-18 22:14:18 +0200

    Finish logic to comply with expected behaviour

--- script.js
+++ script.js
@@ -1,7 +1,66 @@
-let firstNumberInput = 0;
-let secondNumberInput = 0;
-let displayValue = "";
+const DEFAULT_INPUT_VALUE = ""
 
+let inputNumberA = DEFAULT_INPUT_VALUE;
+let inputNumberB = DEFAULT_INPUT_VALUE;
+let firstNumberSet = false;
+let secondNumberSet = false;
+let decimalAlreadyPlaced = false;
+let operatorSelected = false;
+let computedAnswer = 0;
+let lastManualInput = 0;
+
+let currentValue = "";
+
+const displayContainer = document.getElementById("calculator-display")
+const inputBtns = document.querySelectorAll(".input")
+const mathBtns = document.querySelectorAll(".math-functions")
+const clearBtn = document.getElementById("clear")
+const operatorBtn = document.getElementById("equals")
+
+clearBtn.addEventListener('click', () => {
+  clearValuesAndDisplay();
+  removeActiveClassFromButtons();
+});
+
+operatorBtn.addEventListener('click', () => {
+  
+  if (!firstNumberSet){
+    return
+  }
+
+  inputNumberB = currentValue
+  computedAnswer = operate(currentOperator, parseFloat(inputNumberA), parseFloat(inputNumberB));
+  displayContainer.textContent = computedAnswer;
+  inputNumberA = computedAnswer;
+  removeActiveClassFromButtons();
+})
+
+function removeActiveClassFromButtons() {
+  mathBtns.forEach((button) => {
+    button.classList.remove('active');
+  });
+}
+
+function clearValuesAndDisplay() {
+  inputNumberA = DEFAULT_INPUT_VALUE;
+  inputNumberB = DEFAULT_INPUT_VALUE;
+  firstNumberSet = false;
+  secondNumberSet = false;
+  decimalAlreadyPlaced = false;
+  operatorSelected = false;
+  computedAnswer = 0;
+  
+  resetDisplayAndCurrentValue();
+}
+
+function resetDisplayAndCurrentValue() {
+  currentValue = "";
+  displayContainer.textContent = "0";
+}
+
+
+
+// Basic arithmetic operations
 let add = (a, b) => a + b;
 let subract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
@@ -21,3 +80,71 @@ let operate = (operator, firstNumber, secondNumber) => {
     return divide(firstNumber, secondNumber)
   }
 };
+
+function updatecurrentValue(input) {
+  var interpretedInputValue;
+
+  if (input === 'zero' && currentValue === "0") {
+    return;
+  }
+  if (input === 'decimal' && decimalAlreadyPlaced) {
+    return
+  }
+
+  if (input === 'zero') {
+    interpretedInputValue = '0'
+  }
+  else if (input === 'decimal') {
+    if (currentValue === "" || currentValue === "0") {
+      currentValue = "0"
+    }
+    interpretedInputValue = "."
+    decimalAlreadyPlaced = true
+  }
+  else {
+    interpretedInputValue = input
+  }
+  
+  currentValue += interpretedInputValue
+  displayContainer.textContent = currentValue
+}
+
+function saveCurrentNumber(value) {
+  if (firstNumberSet) {
+    inputNumberB = value;
+    secondNumberSet = true;
+  }
+  else {
+    inputNumberA = value
+    firstNumberSet = true;
+  }
+}
+
+inputBtns.forEach((button) => {
+  button.addEventListener('click', e => {
+    updatecurrentValue(e.target.id)
+  })
+});
+
+mathBtns.forEach((button) => {
+  button.addEventListener('click', e => {
+    currentOperator = e.target.id
+
+    if (!firstNumberSet) {
+      saveCurrentNumber(currentValue)
+      resetDisplayAndCurrentValue()
+      displayContainer.textContent = inputNumberA
+    }
+
+    if (!operatorSelected) {
+      button.classList.add('active');
+      operatorSelected = true;
+    }
+    else {
+      removeActiveClassFromButtons()
+      button.classList.add('active')
+    }
+    
+    
+  })
+})
